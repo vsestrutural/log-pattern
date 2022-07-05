7b0a90d765f0db7d73da71aae0e5db0b7f7f60af
Fujioka <rodrigo.a.fujioka@capgemini.com>
2022-07-05 03:22:54 -0300

    App for charpter by VS Estrutural

---
+++ src/main/java/com/capgemini/vsestrutural/charpter/LogPatternApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.vsestrutural.charpter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogPatternApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capgemini/vsestrutural/charpter/LogPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.vsestrutural.charpter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogPatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
